d2e038ece1a6365cf36af3909674348a87b4cd32
Radhika <radhika@techenvision.in>
2024-04-25 19:16:54 +0530

    changes
    changes

---
+++ app/Http/Controllers/admin/FaqController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FaqController extends Controller
+{
+    public function index(){
+        $result = Faq::orderBy('id','DESC')->get();
+        return view('admin/faq/list',compact('result'));
+    }
+
+    public function add(){
+        return view('admin/faq/add');
+    }
+
+    public function save(Request $req){
+        // echo "<pre>";
+        // print_r($req->all());die;
+        $faq = new Faq();
+        $faq->question = $req->question;
+        $faq->answer = $req->answer;
+        $faq->save();
+    }
+}
---
+++ resources/views/admin/faq/add.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+@section('title')
+@lang('translation.Basic_Elements')
+@endsection
+
+@section('content')
+@component('common-components.breadcrumb')
+@slot('pagetitle') Forms @endslot
+@slot('title') Basic Elements @endslot
+@endcomponent
+
+
+
+@endsection
